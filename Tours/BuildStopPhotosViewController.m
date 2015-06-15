75002c09b66dd3417a9e4188194a1cd8abd867f3
Mark Porcella <markporcella67@gmail.com>
2015-06-15 15:15:31 -0700

    adding buttons and action sheet

--- Tours/BuildStopPhotosViewController.m
+++ Tours/BuildStopPhotosViewController.m
@@ -29,6 +29,33 @@
 }
 
 
+
+- (IBAction)onAddPictureButtonPressed:(UIButton *)sender {
+
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"ActionTitle" message:@"ActionMessage" preferredStyle:UIAlertControllerStyleActionSheet];
+
+    UIAlertAction *takePictureAction = [UIAlertAction actionWithTitle:@"Take a Picture" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        // Here you are going to write the code
+        NSLog(@"Take Picture Action");
+    }];
+
+    UIAlertAction *photoLibraryAction = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        // Here you are going to write the code
+        NSLog(@"Photo Library Action");
+    }];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        NSLog(@"Cancel Action");
+    }];
+
+
+    [alertController addAction:takePictureAction];
+    [alertController addAction:photoLibraryAction];
+    [alertController addAction:cancelAction];
+
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
 //- (void) showPhotoLibraryPikcerController {
 //    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
 //    picker.delegate = self;
---
+++ Tours/ImagePickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagePickerViewController.h
+//  Tours
+//
+//  Created by Mark Porcella on 6/15/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePickerViewController : UIViewController
+
+@end
