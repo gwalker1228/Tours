43adf83a2538e7f1badb7dc84defcd77059fb59f
gwalker1228 <gwalker1228@gmail.com>
2015-06-16 18:56:02 -0700

    adding custom table view cell to BuildTourStopsViewController with nested custom collection view for displaying/scrolling through stop photos. still need to load from actual Stop objects

--- Tours/BuildTourStopsViewController.m
+++ Tours/BuildTourStopsViewController.m
@@ -12,6 +12,7 @@
 #import "IndexedPhotoCollectionViewCell.h"
 #import "Stop.h"
 
+
 @interface CustomClass : NSObject
 
 @property NSString *title;
@@ -52,6 +53,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+
     BuildManager *buildManager = [BuildManager sharedBuildManager];
     self.tour = buildManager.tour;
 
@@ -67,35 +69,10 @@
     CustomClass *object5 = [[CustomClass alloc] initWithTitle:@"Title object 5" detail:@"Description number 5"
                                                        photos:@[@"1", @"2", @"3", @"4",@"5", @"6", @"7"]];
     self.objects = [NSArray arrayWithObjects:object1, object2, object3, object4, object5, nil];
+
+    [self.tableView reloadData];
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-
-
-
-    return cell;
-
-
-//    BuildTourStopsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-//    [cell setCollectionViewDataSourceDelegate:self indexPath:indexPath];
-//
-//    CustomClass *object = self.objects[indexPath.row];
-//
-//    cell.titleLabel.text = object.title;
-//    cell.descriptionLabel.text = object.detail;
-//
-//    return cell;
-}
-
-#pragma mark - UITableViewDataSource & Delegate
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-
-    return self.objects.count;
-   // return self.stops.count;
-}
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 
@@ -108,31 +85,77 @@
     [stop saveInBackground];
 }
 
+
+#pragma mark - UITableViewDataSource & Delegate
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+//
+//
+//
+//    return cell;
+
+    BuildTourStopsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+    [cell setCollectionViewDataSourceDelegate:self indexPath:indexPath];
+
+    CustomClass *object = self.objects[indexPath.row];
+
+    cell.titleLabel.text = object.title;
+    cell.descriptionLabel.text = object.detail;
+
+    return cell;
+}
+
+//-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+//
+//    BuildTourStopsTableViewCell *tableCell = (BuildTourStopsTableViewCell *)cell;
+//
+//    [tableCell setCollectionViewDataSourceDelegate:self indexPath:indexPath];
+//
+//    CustomClass *object = self.objects[indexPath.row];
+//
+//    tableCell.titleLabel.text = object.title;
+//    tableCell.descriptionLabel.text = object.detail;
+//}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return tableCellHeight;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.objects.count;
+   // return self.stops.count;
+}
+
+
 #pragma mark - UICollectionViewDataSource, Delegate & DelegateFlowLayout
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
 
-//    CustomClass *object = self.objects[[(IndexedPhotoCollectionView *)collectionView indexPath].row];
-//    NSArray *collectionViewArray = object.photos;
-//    return collectionViewArray.count;
-    return 0;
+    CustomClass *object = self.objects[[(IndexedPhotoCollectionView *)collectionView indexPath].row];
+    NSArray *collectionViewArray = object.photos;
+    return collectionViewArray.count;
+//    return 0;
 }
 
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
 
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-
-    return cell;
-//    IndexedPhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:indexedPhotoCollectionViewCellID forIndexPath:indexPath];
-//
-//    CustomClass *object = self.objects[[(IndexedPhotoCollectionView *)collectionView indexPath].row];
-//    NSString *fileName = object.photos[indexPath.row];
-//
-//    cell.backgroundColor = [UIColor whiteColor];
-//    cell.imageView.image = [UIImage imageNamed:fileName];
+//    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
 //
 //    return cell;
+    IndexedPhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:indexedPhotoCollectionViewCellID forIndexPath:indexPath];
+
+    CustomClass *object = self.objects[[(IndexedPhotoCollectionView *)collectionView indexPath].row];
+    NSString *fileName = object.photos[indexPath.row];
+
+   // cell.backgroundColor = [UIColor redColor];
+    cell.imageView.image = [UIImage imageNamed:fileName];
+
+    return cell;
 }
 
 @end
