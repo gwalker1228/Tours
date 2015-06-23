be01594711f7fcd55c514f5f16d8e4a2d53f9706
Mark Porcella <markporcella67@gmail.com>
2015-06-23 14:11:01 -0700

    adding star rating object

---
+++ Tours/RateView.h
@@ -0,0 +1,34 @@
+//
+//  RatingStar.h
+//  Tours
+//
+//  Created by Mark Porcella on 6/23/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RateView;
+
+@protocol RateViewDelegate <NSObject>
+
+-(void)rateView:(RateView *)rateView ratingDidChange:(float)rating;
+
+@end
+
+@interface RateView : UIView
+
+@property (strong, nonatomic) UIImage *notSelectedImage;
+@property (strong, nonatomic) UIImage *halfSelectedImage;
+@property (strong, nonatomic) UIImage *fullSelectedImage;
+@property (assign, nonatomic) float rating;
+@property (assign) BOOL editable;
+@property (strong) NSMutableArray * imageViews;
+@property (assign, nonatomic) int maxRating;
+@property (assign) int midMargin;
+@property (assign) int leftMargin;
+@property (assign) CGSize minImageSize;
+
+@property (assign) id <RateViewDelegate> delegate;
+
+@end
